3385bc878181c59741ba43cf04487ff4c88ee95d
rashidfirhat98 <rashidfirhat98@gmail.com>
2020-09-15 23:49:32 +0800

    Chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   validates :name, presence: true,
     length: {maximum: Settings.user.name.max_length}
 
@@ -15,11 +17,31 @@ class User < ApplicationRecord
   before_save :downcase_email
 
   class << User
-    def digest string
-      cost = ActiveModel::SecurePassword.min_cost ? Bcrypt::Engine::MIN_COST :
-                                                  Bcrypt::Engine.cost
-      Bcrypt::Password.create string, cost: cost
+    def new_token
+      SecureRandom.urlsafe_base64
     end
+
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+              BCrypt::Engine::MIN_COST
+            else
+              BCrypt::Engine.cost
+            end
+      BCrypt::Password.create string, cost: cost
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
   end
 
   private
