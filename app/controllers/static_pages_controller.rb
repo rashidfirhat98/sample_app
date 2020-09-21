10240c0480fcfe4f6b6df3655f27648ce03e8204
rashidfirhat98 <rashidfirhat98@gmail.com>
2020-09-22 00:10:27 +0800

    Chapter 13

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.page(params[:page]).per Settings.paging
+    end
+  end
 
   def help; end
 
---
+++ app/models/micropost.rb
@@ -0,0 +1,20 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  scope :by_created_at, -> { order(created_at: :desc) }
+
+  validates :user_id, presence: true
+
+  validates :content, presence: true, 
+  length: {maximum: Settings.validate.post.max_length}
+
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
+                                  message: "must be a valid image format" },
+                           size: { less_than: 5.megabytes,
+                                  message: "should be less than 5MB" }
+  
+  def display_image
+    image.variant resize_to_limit: [500, 500]
+  end       
+end
